6309def308a6bcfef9d34d2e1f073b367f3b2255
ndriqa <ndriqimrrahmani@hotmail.com>
2021-05-14 14:07:40 +0200

    init files

---
+++ index.php
@@ -0,0 +1,275 @@
+<!DOCTYPE html>
+<html lang="en" manifest="https://ndriqa.github.io/PWWW20-21_Gr10/index.html">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+    integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA=="
+    crossorigin="anonymous" />
+  <link rel="stylesheet" type="text/css" href="./style/style.css">
+  <title>Freshness</title>
+
+  <!--needed font-->
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Old+Standard+TT&display=swap&weight=200" rel="stylesheet">
+</head>
+
+<body class="nm-bg">
+  <!--HEADER STUFF-->
+  <header class="nm-bg">
+    <div class="social-top nm-bg">
+      <div class="social-top-content">
+        <div class="social-icons">
+          <a href="https://www.linkedin.com/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+          <a href="https://www.twitter.com/" target="_blank"><i class="fab fa-twitter"></i></a>
+          <a href="https://www.pinterest.com/" target="_blank"><i class="fab fa-pinterest"></i></a>
+          <a href="https://www.google.com/" target="_blank"><i class="fab fa-google-plus-g"></i></a>
+          <a href="https://www.rss.com" target="_blank"><i class="fas fa-rss-square"></i></a>
+        </div>
+
+        <div>
+          <button id="theme-switcher">Change theme</button>
+          <span id="verticalLine"></span>
+          <input type="text" name="" id="searchBox" placeholder="Search Our Website...." />
+          <input type="submit" value="SEARCH" />
+        </div>
+      </div>
+    </div>
+
+    <div class="the-header">
+      <div class="homepage-description nm-bg">
+        <h1 class="nm-bg">
+          Freshness
+        </h1>
+        <p class="nm-bg">
+          Eat healthy, eat fresh!
+        </p>
+      </div>
+
+      <nav class="header-nav-bar ">
+        <ul id="menu">
+          <li class="nav-item"><a class="nav-link nav-active" href="./index.html" >HOME</a> </li>
+          <li class="nav-item"><a class="nav-link nm-bg" href="./about.html" >ABOUT US </a></li>
+          <li class="nav-item"><a class="nav-link nm-bg" href="./meny.html" >MENU </a></li>
+          <li class="nav-item"><a class="nav-link nm-bg" href="./order.html" >ORDER NOW </a></li>
+          <li class="nav-item"><a class="nav-link nm-bg" href="./reservations.html" >RESERVATION </a></li>
+          <li class="nav-item">
+            <div class="dropdown">
+              <a class="nm-bg">OUR STAFF</a>
+              <div class="dropdown-content">
+                <a href="./board.html" class="nav-link nm-bg" >BOARD</a> <hr>
+                <a href="./chefs.html" class="nav-link nm-bg" >CHEFS</a>
+              </div>
+            </div>
+          </li>
+          <li class="nav-item"><a class="nav-link nm-bg" href="./contact.html">CONTACT </a></li>
+        </ul>
+      </nav> 
+    </div>
+  </header>
+
+  <hr class="separator">
+
+  <!--ACTUAL PAGE STUFF-->
+  <main>
+    
+    <div id="info" class="sections">
+
+      <div class="dishes-panel">
+        <!--Image 1-->
+        <img id="dish-image" src="./Images/homepage/dish1.png" alt="chicken special" class="dish">
+      </div>
+
+      <div class="dishes-slides">
+        <!--Image 1 text-->
+        <div class="my-slides-text fade">
+          <div class="dish-intro">
+            <h1 class="nm-bg" id="dish-name">
+              Lazanje
+            </h1>
+            <div class="slide-buttons-panel">
+              <button class="slide-button"  onclick="forcedPrev()">&lt;</button>
+              <button class="slide-button" onclick="forcedNext()">&gt;</button>
+            </div>
+          </div>
+          <p class="nm-bg" id="dish-description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Diam volutpat commodo sed egestas egestas.
+            Eleifend mi in nulla posuere sollicitudin.
+            Dictum sit amet justo donec enim diam vulputate ut pharetra.
+          </p>
+        </div>
+        <div class="dots-div">
+          <button>READ MORE &#187;</button>
+        <div class="dots">
+          <span class="dot" onclick="changeSlide(1)"></span>
+          <span class="dot" onclick="changeSlide(2)"></span>
+          <span class="dot" onclick="changeSlide(3)"></span>
+          <span class="dot" onclick="changeSlide(4)"></span>
+        </div>
+        </div>
+      </div>
+    </div>
+    <hr>
+
+    <div id="speciality-dishes" class="speciality-section ">
+
+      <div class="speciality">
+        <img src="./Images/homepage/speciality1.jpg" alt="speciality dish image" class="speciality-image">
+        <h1 class="nm-bg">Steak tartare</h1>
+        <p class="nm-bg">It consists of Beef tenderloin, Worcerster sauce, Pickled cucumber.
+        </p>
+        <button>READ MORE &#187;</button>
+      </div>
+      <div class="speciality">
+        <img src="./Images/homepage/speciality2.jpg" alt="speciality dish image" class="speciality-image">
+        <h1 class="nm-bg">Parmigiana</h1>
+        <p class="nm-bg">It consists of Chicken breasts, italian, mozzarella cheese, Parmesan cheese.
+        </p>
+        <button>READ MORE &#187;</button>
+      </div>
+      <div class="speciality">
+        <img src="./Images/homepage/speciality3.jpg" alt="speciality dish image" class="speciality-image">
+        <h1 class="nm-bg">Barbecue Ribs</h1>
+        <p class="nm-bg">It consists of Pork, Barbecue sauce, Chilli.
+        </p>
+        <button>READ MORE &#187;</button>
+      </div>
+
+    </div>
+    <hr>
+    <div id="critics-opinions" class="sections">
+      <div>
+        <p class="deep-thought nm-bg"><em><strong>"Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit.
+              In nisl nisi, maximus nec malesuada in,
+              commodo ac orci.
+              Fusce facilisis eros sed nisi consectetur faucibus"
+            </strong></em></p>
+      </div>
+      <div class="opinions">
+        <div class="critic">
+          <div class="avatar">
+            <img src="./Images/homepage/avatar/m.png" alt="critic face">
+          </div>
+          <div class="opinion">
+            <a href="#">Hakeem Lacey</a>
+            <p class="nm-bg">This is the best restaurant in the west of Kosovo.
+              I recommend all to go there</p>
+          </div>
+        </div>
+        <div class="critic">
+          <div class="avatar">
+            <img src="./Images/homepage/avatar/f.png" alt="critic face">
+          </div>
+          <div class="opinion">
+            <a href="#">Anjali Rees</a>
+            <p class="nm-bg">This is the best restaurant in the west of Kosovo.
+              I recommend all to go there</p>
+          </div>
+        </div>
+        <div class="critic">
+          <div class="avatar">
+            <img src="./Images/homepage/avatar/m.png" alt="critic face">
+          </div>
+          <div class="opinion">
+            <a href="#">Tariq Walters</a>
+            <p class="nm-bg">This is the best restaurant in the west of Kosovo.
+              I recommend all to go there</p>
+          </div>
+        </div>
+      </div>
+      <div class="opinions">
+        <div class="critic">
+          <div class="avatar">
+            <img src="./Images/homepage/avatar/f.png" alt="critic face">
+          </div>
+          <div class="opinion">
+            <a href="#">Raja Davison</a>
+            <p class="nm-bg">This is the best restaurant in the west of Kosovo.
+              I recommend all to go there</p>
+          </div>
+        </div>
+        <div class="critic">
+          <div class="avatar">
+            <img src="./Images/homepage/avatar/m.png" alt="critic face">
+          </div>
+          <div class="opinion">
+            <a href="#">Hassan Dowling</a>
+            <p class="nm-bg">This is the best restaurant in the west of Kosovo.
+              I recommend all to go there</p>
+          </div>
+        </div>
+        <div class="critic">
+          <div class="avatar">
+            <img src="./Images/homepage/avatar/f.png" alt="critic face">
+          </div>
+          <div class="opinion">
+            <a href="#">Rikki Mccall</a>
+            <p class="nm-bg">This is the best restaurant in the west of Kosovo.
+              I recommend all to go there</p>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+    <br>
+  </main>
+  <!--FOOTER STUFF-->
+  <footer>
+    <div class="main-footer">
+      <div id="contact-details" class="sector">
+        <h1>Contact Details </h1>
+        <pre>
+Freshness
+Dardania
+Prishtine
+10000
+        </pre>
+        <p>Tel: +383 45 123 456</p>
+        <p>Fax: 1234 56789</p>
+        <p>Email: <a href="mailto::username@domain.com">username@domain.com</a></p>
+      </div>
+      <div id="quick-links" class="sector">
+        <h1>Quick Links</h1>
+        <a href="#">Our specialities</a>
+        <hr class="sep">
+        <a href="#">Our wonderful staff</a>
+        <hr class="sep">
+        <a href="#">Make a reservation</a>
+        <hr class="sep">
+        <a href="#">Order now</a>
+        <hr class="sep">
+      </div>
+      <div id="from-the-blog" class="sector">
+        <h1>From The Blog</h1>
+        <pre>
+<strong>Preparing Tuna Fish</strong>
+<small>Posted by </small>Admin on 00.00.0000
+This is usually a coast side 
+dish with the calories needed
+to last throughout the day
+        </pre>
+        <a href="#" style="float: right;">See More &#187;</a>
+      </div>
+      <div id="newsletter" class="sector">
+        <h1>Newsletter</h1>
+        <input type="text" placeholder="Name">
+        <input type="text" placeholder="Email">
+        <input type="submit" style="font-weight: bold;">
+
+      </div>
+    </div>
+    <div class="end-footer">
+      <p>Copyright &copy; <sub>2013</sub> Domain Name - All Rights Reserved</p>
+      <p>Made By Group 10</p>
+    </div>
+  </footer>
+</body>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script src="./js/script.js"></script>
+<script src="./js/nightmode.js"></script>
+</html>
---
+++ order.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html lang="en" manifest="https://ndriqa.github.io/PWWW20-21_Gr10/order.html">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+    integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA=="
+    crossorigin="anonymous" />
+  <link rel="stylesheet" type="text/css" href="style/style.css">
+  <link rel="stylesheet" type="text/css" href="style/order_style.css">
+  <title>Order Now</title>
+</head>
+
+<body class="nm-bg">
+  <!--HEADER STUFF-->
+  <header>
+    <div class="social-top nm-bg">
+      <div class="social-top-content">
+        <div class="social-icons">
+          <a href="https://www.linkedin.com/"><i class="fab fa-linkedin-in"></i></a>
+          <a href="https://twitter.com/"><i class="fab fa-twitter"></i></a>
+          <a href="https://www.pinterest.com/"><i class="fab fa-pinterest"></i></a>
+          <a href="https://www.google.com/"><i class="fab fa-google-plus-g"></i></a>
+          <a href="https://www.rss.com"><i class="fas fa-rss-square"></i></a>
+        </div>
+
+        <div>
+          <input type="text" name="" id="searchBox" placeholder="Search Our Website...." />
+          <input type="submit" value="SEARCH" />
+        </div>
+      </div>
+    </div>
+
+    <div class="the-header">
+      <div class="homepage-description">
+        <h1 class="nm-bg">
+          Freshness
+        </h1>
+        <p class="nm-bg">
+          Eat healthy, eat fresh!
+        </p>
+      </div>
+
+      <nav class="header-nav-bar ">
+        <ul id="menu">
+          <li class="nav-item"><a class="nav-link nm-bg" href="./index.html" class="nav-link">HOME</a> </li>
+          <li class="nav-item"><a class="nav-link nm-bg" href="./about.html">ABOUT US </a></li>
+          <li class="nav-item"><a class="nav-link nm-bg" href="./meny.html">MENU </a></li>
+          <li class="nav-item"><a class="nav-link nav-active" href="./order.html">ORDER NOW </a></li>
+          <li class="nav-item"><a class="nav-link nm-bg" href="./reservations.html">RESERVATION </a>
+            <li class="nav-item">
+              <div class="dropdown">
+                <a class="nm-bg">OUR STAFF</a>
+                <div class="dropdown-content">
+                  <a class="nav-link nm-bg" href="./board.html">BOARD</a> <hr>
+                  <a class="nav-link nm-bg" href="./chefs.html">CHEFS</a>
+                </div>
+              </div>
+            </li>
+            <li class="nav-item nm-bg"><a class="nav-link" href="./contact.html">CONTACT </a>
+        </ul>
+      </nav>
+    </div>
+  </header>
+
+  <hr class="separator">
+
+  <!--ACTUAL PAGE STUFF-->
+  <main>
+    <div class="order-message">
+      <p class="message nm-bg"><em><strong>
+            Tell us what you want to eat and we'll prepare and deliver it to your address!
+          </strong></em></p>
+    </div>
+    <form id="porosia" onsubmit="return false;">
+      <fieldset id="lloji_i_porosise">
+        <div class="form-section">
+          <label for="foods">Food type:</label>
+          <select id="foods" name="foods">
+            <option value="breakfast">Breakfast</option>
+            <option value="lunch">Lunch</option>
+            <option value="pizzapasta">Pizza and Pasta</option>
+            <option value="specials">Specials</option>
+          </select>
+          <button id="choose" type="button">Choose</button>
+        </div>
+      </fieldset>
+      <div id="intro">
+        <div id="arrow">
+          <div id="maja"></div>
+        </div>
+        <div>
+          <h3 class="message nm-bg">Choose the type of food you want, here.</h3>
+        </div>
+      </div>
+
+      <div id="ushqimeParent">
+        <h2 class="message nm-bg">What do you want to eat?</h2>
+        <fieldset id="ushqimet">
+        </fieldset>
+      </div>
+
+      <div id="pijeParent">
+        <h2 class="message nm-bg">How about a drink?</h2>
+        <fieldset id="pijet">
+
+        </fieldset>
+      </div>
+      <fieldset id="perzgjedhja" class="hidden">
+        <div id="cart_images">
+          <div>
+            <img id="chosen_food" alt="food">
+          </div>
+          <div>
+            <img id="chosen_drink" alt="drink">
+          </div>
+        </div>
+        <div id="cart">
+          <h3 class="message" id="food_name"></h3>
+          <h2 id="food_price"></h2>
+          <h3>+</h3>
+          <h3 class="message" id="drink_name"></h3>
+          <h2 id="drink_price"></h2>
+          <hr>
+          <h3>Total</h3>
+          <h2 class="message" id="total_price"></h2>
+        </div>
+        <div id="user_info">
+          <div id="user_name">
+            <label for="fname" >First name:</label>
+            <input type="text" id="fname" name="fname" required>
+            <label for="lname">Last name:</label>
+            <input type="text" id="lname" name="lname" required>
+            <label for="num">Number:</label>
+            <input type="text" id="num" name="num" required>
+            <label for="address">Address:</label>
+            <input type="text" id="address" name="address" required><br>
+                      
+          </div>
+          <div id="user_submit">
+            <h4 class="message">Payment</h4>
+            <input type="radio" id="cash" name="payment" value="cash">
+            <label for="cash">Cash</label>
+            <input type="radio" id="paypal" name="payment" value="paypal">
+            <label for="paypal">Paypal</label>
+            <input type="radio" id="card" name="payment" value="card">
+            <label for="card">Credit Card</label>
+            <input type="radio" id="other" name="payment" value="other">
+            <label for="other">Other</label> <br>
+          
+            <button id="cash_out" onclick="cashout()" type="button">Submit</button>
+          </div>
+        </div>
+
+      </fieldset>
+    </form>
+  </main>
+
+  <!--FOOTER STUFF-->
+  <footer>
+    <div class="main-footer">
+      <div id="contact-details" class="sector">
+        <h1>Contact Details </h1>
+        <pre>
+Freshness
+Dardania
+Prishtine
+10000
+        </pre>
+        <p>Tel: +383 45 123 456</p>
+        <p>Fax: 1234 56789</p>
+        <p>Email: <a href="mailto::username@domain.com">username@domain.com</a></p>
+      </div>
+      <div id="quick-links" class="sector">
+        <h1>Quick Links</h1>
+        <a href="#">Our specialities</a>
+        <hr class="sep">
+        <a href="#">Our wonderful staff</a>
+        <hr class="sep">
+        <a href="#">Make a reservation</a>
+        <hr class="sep">
+        <a href="#">Order now</a>
+        <hr class="sep">
+      </div>
+      <div id="from-the-blog" class="sector">
+        <h1>From The Blog</h1>
+        <pre>
+<strong>Preparing Tuna Fish</strong>
+<small>Posted by </small>Admin on 00.00.0000
+This is usually a coast side 
+dish with the calories needed
+to last throughout the day
+        </pre>
+        <a href="#" style="float: right;">See More &#187;</a>
+      </div>
+      <div id="newsletter" class="sector">
+        <h1>Newsletter</h1>
+        <input type="text" placeholder="Name">
+        <input type="text" placeholder="Email">
+        <input type="submit" style="font-weight: bold;">
+
+      </div>
+    </div>
+    <div class="end-footer">
+      <p>Copyright &copy; <sub>2013</sub> Domain Name - All Rights Reserved</p>
+      <p>Made By Group 10</p>
+    </div>
+  </footer>
+  <script src="./js/order.js"></script>
+  <script src="./js/nightmode_order.js"></script>
+</body>
+
+</html>
