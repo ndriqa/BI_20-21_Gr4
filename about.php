60c2744c6ab5649a2da428956e24851d2e569112
Urata Sadiku <sadikuurata@gmail.com>
2021-05-14 14:31:49 +0200

    Add files css,js and php files

---
+++ about.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html lang="en" manifest="https://ndriqa.github.io/PWWW20-21_Gr10/about.html">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+    integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA=="
+    crossorigin="anonymous" />
+  <link rel="stylesheet" type="text/css" href="style/style.css">
+  <link rel="stylesheet" type="text/css" href="style/about.css">
+
+  <!--needed font-->
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Old+Standard+TT&display=swap&weight=200" rel="stylesheet">
+  <title>About Us</title>
+</head>
+
+<body class="nm-bg">
+
+  <!--HEADER STUFF-->
+  <header class="night-background">
+    <div class="social-top nm-bg">
+      <div class="social-top-content">
+        <div class="social-icons">
+          <a href="https://www.linkedin.com/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+          <a href="https://www.twitter.com/" target="_blank"><i class="fab fa-twitter"></i></a>
+          <a href="https://www.pinterest.com/" target="_blank"><i class="fab fa-pinterest"></i></a>
+          <a href="https://www.google.com/" target="_blank"><i class="fab fa-google-plus-g"></i></a>
+          <a href="https://www.rss.com" target="_blank"><i class="fas fa-rss-square"></i></a>
+        </div>
+
+        <div>
+          <input type="text" name="" id="searchBox" placeholder="Search Our Website...." />
+          <input type="submit" value="SEARCH" />
+        </div>
+      </div>
+    </div>
+
+    <div class="the-header">
+      <div class="homepage-description">
+        <h1 class="nm-bg">
+          Freshness
+        </h1>
+        <p class="nm-bg">
+          Eat healthy, eat fresh!
+        </p>
+      </div>
+
+      <nav class="header-nav-bar ">
+        <ul id="menu">
+          <li class="nav-item"><a class="nav-link nm-bg" href="./index.php" >HOME</a> </li>
+          <li class="nav-item"><a class="nav-link nav-active " href="./about.php" >ABOUT US </a></li>
+          <li class="nav-item"><a class="nav-link nm-bg " href="./meny.php" >MENU </a></li>
+          <li class="nav-item"><a class="nav-link nm-bg " href="./order.php" >ORDER NOW </a></li>
+          <li class="nav-item"><a class="nav-link nm-bg " href="./reservations.php" >RESERVATION </a></li>
+          <li class="nav-item">
+            <div class="dropdown">
+              <a  class="nm-bg">OUR STAFF</a>
+              <div class="dropdown-content">
+                <a href="./board.php" class="nav-link nm-bg " >BOARD</a> <hr>
+                <a href="./chefs.php" class="nav-link nm-bg " >CHEFS</a>
+              </div>
+            </div>
+          </li>
+          <li class="nav-item"><a class="nav-link nm-bg " href="./contact.php">CONTACT </a></li>
+        </ul>
+      </nav> 
+    </div>
+  </header>
+
+  <hr class="separator">
+<main>
+  <div class="main">
+    <div class="about">
+      <div class="about-section">
+        <div class="about-background"></div>
+        <div class="inner-container">
+          <div>
+            <h1>About Us</h1>
+            <p class="text">
+              At <strong>FRESHNESS</strong>, we create good memories by serving original-style deep-dish pizza using a recipe
+              that has been passed down over generations.
+              Our restaurant provides an authentic dining experience that is unique to the area. 'Freshness' was worked for
+              people in general and has advanced into a combination between exquisite chic and contemporary fine charge.
+            </p>
+            <div class="skills">
+              <span style="color: black;">Enjoy our dazzling dishes and make the most of your eating background with us!</span>
+              <button><a href="https://www.google.com/" style="color: white;">Read more >></a></button>
+            </div>  
+          </div>
+        </div>
+      </div>  
+    </div>
+    <br>
+  
+    <h3 id="h3" style="text-align:center; font-size: 2.5rem;
+      text-align: center;
+      font-weight: bold;
+      font-style: italic;
+      color: var(--our-darker-cyan);">Happy Clients</h3>
+    <section id="happy_clients">
+      <div class="ins">
+  
+        <div class="card2">
+          <div class="container">
+            <img src="./Images/about/clients/team1.jpg" alt="client" />
+          </div>
+  
+          <div class="team">
+            <h4 id="hov1">Jane Doe</h4>
+            <audio controls>
+              <source src="./audio/1.mp3" type="audio/mpeg">
+            </audio>
+  
+          </div>
+        </div>
+        <div class="card2">
+          <div class="container">
+            <img src="./Images/about/clients/team2.jpg" alt="client" />
+          </div>
+  
+  
+          <div class="team">
+            <h4 id="hov2">Mike Ross</h4>
+            <audio controls>
+              <source src="./audio/2.mp3" type="audio/mpeg">
+            </audio>
+          </div>
+        </div>
+        <div class="card2">
+          <div class="container">
+            <img src="./Images/about/clients/team3.jpg" alt="client" />
+          </div>
+          <div class="team">
+            <h4 id="hov3">Fabiano Caruana</h4>
+            <audio controls>
+              <source src="./audio/3.mp3" type="audio/mpeg">
+            </audio>
+          </div><br>
+        </div>
+        </div>
+      </section>
+        <div class="restaurant">
+          <h3 id="h3" style="text-align:center; font-size: 2.5rem;
+          text-align: center;
+          font-weight: bold;
+          font-style: italic;
+          color: var(--our-darker-cyan);">Our specialities? </h3>
+          <h4 style="text-align:center; font-size: 1.5rem;
+          text-align: center;
+          font-weight: bold;
+          font-style: italic;
+          color: var(--our-darker-cyan);">Watch them in the video below :) </h4>
+          <video width="700" height="500" controls>
+            <source src="./video/Shots of Different Food.mp4" type="video/mp4">
+          Your browser does not support the video tag.
+          </video>
+        </div>
+    </section>
+    </div>
+  </main>
+
+  <!--FOOTER STUFF-->
+  <footer>
+    <div class="main-footer">
+      <div id="contact-details" class="sector">
+        <h1>Contact Details </h1>
+        <pre>
+Freshness
+Dardania
+Prishtine
+10000
+        </pre>
+        <p>Tel: +383 45 123 456</p>
+        <p>Fax: 1234 56789</p>
+        <p>Email: <a href="mailto::username@domain.com">username@domain.com</a></p>
+      </div>
+      <div id="quick-links" class="sector">
+        <h1>Quick Links</h1>
+        <a href="#">Our specialities</a>
+        <hr class="sep">
+        <a href="#">Our wonderful staff</a>
+        <hr class="sep">
+        <a href="#">Make a reservation</a>
+        <hr class="sep">
+        <a href="#">Order now</a>
+        <hr class="sep">
+      </div>
+      <div id="from-the-blog" class="sector">
+        <h1>From The Blog</h1>
+        <pre>
+<strong>Preparing Tuna Fish</strong>
+<small>Posted by </small>Admin on 00.00.0000
+This is usually a coast side 
+dish with the calories needed
+to last throughout the day
+        </pre>
+        <a href="#" style="float: right;">See More &#187;</a>
+      </div>
+      <div id="newsletter" class="sector">
+        <h1>Newsletter</h1>
+        <input type="text" placeholder="Name">
+        <input type="text" placeholder="Email">
+        <input type="submit" style="font-weight: bold;">
+
+      </div>
+    </div>
+    <div class="end-footer">
+      <p>Copyright &copy; <sub>2013</sub> Domain Name - All Rights Reserved</p>
+      <p>Made By Group 10</p>
+    </div>
+  </footer>
+  <script src="./js/nightmode_about.js"></script>
+</body>
+
+</html>
---
+++ contact.php
@@ -0,0 +1,305 @@
+<!DOCTYPE html>
+<html lang="en" manifest="https://ndriqa.github.io/PWWW20-21_Gr10/contact.html">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+    integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA=="
+    crossorigin="anonymous" />
+  <link rel="stylesheet" type="text/css" href="style/contact.css">
+  <link rel="stylesheet" href="./style/style.css">
+  <title>Freshness</title>
+  <script src="./js/contact.js"></script>
+
+  <!--for testing purposes-->
+  <!-- <meta http-equiv="refresh" content="1"> -->
+
+  <!--needed font-->
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Old+Standard+TT&display=swap&weight=200" rel="stylesheet">
+</head>
+
+<body>
+   <!--HEADER STUFF-->
+   <header >
+    <div class="social-top">
+      <div class="social-top-content">
+        <div class="social-icons">
+          <a href="https://www.linkedin.com/"
+            ><i class="fab fa-linkedin-in"></i
+          ></a>
+          <a href="https://twitter.com/"><i class="fab fa-twitter"></i></a>
+          <a href="https://www.pinterest.com/"
+            ><i class="fab fa-pinterest"></i
+          ></a>
+          <a href="https://www.google.com/"
+            ><i class="fab fa-google-plus-g"></i
+          ></a>
+          <a href="https://www.rss.com"><i class="fas fa-rss-square"></i></a>
+        </div>
+
+        <div>
+          <input type="text"
+            name=""id="searchBox"placeholder="Search Our Website...."/>
+          <input type="submit" value="SEARCH" />
+        </div>
+      </div>
+    </div>
+
+    <div class="the-header">
+      <div class="homepage-description">
+        <h1>Freshness</h1>
+        <p>Eat healthy, eat fresh!</p>
+      </div>
+
+      <nav class="header-nav-bar ">
+        <ul id="menu">
+          <li class="nav-item"><a class="nav-link " href="./index.php" >HOME</a> </li>
+          <li class="nav-item"><a class="nav-link " href="./about.php" >ABOUT US </a></li>
+          <li class="nav-item"><a class="nav-link " href="./meny.php" >MENU </a></li>
+          <li class="nav-item"><a class="nav-link " href="./order.php" >ORDER NOW </a></li>
+          <li class="nav-item"><a class="nav-link " href="./reservations.php" >RESERVATION </a></li>
+          <li class="nav-item">
+            <div class="dropdown">
+              <a>OUR STAFF</a>
+              <div class="dropdown-content">
+                <a href="./board.php" class="nav-link " >BOARD</a> <hr>
+                <a href="./chefs.php" class="nav-link " >CHEFS</a>
+              </div>
+            </div>
+          </li>
+          <li class="nav-item"><a class="nav-link nav-active" href="./contact.php">CONTACT </a></li>
+        </ul>
+      </nav> 
+    </div>
+  </header>
+
+  <hr class="separator"/>
+   <!--ACTUAL PAGE STUFF-->
+   <main>
+    <div class="container">
+     
+      <section class="section1">
+        <div class="sec1title">
+          <h1 class="text-message">Get in touch</h1>
+        </div>
+      </section>
+      
+
+      <div class="wrapper">
+        <!-- RESTAURANT INFORMATION -->
+        <div class="company-info">
+          <h3>Freshness restaurant</h3>
+
+          <ul>
+            <li><i class="fa fa-road"></i> 44,Dardania, Prishtine</li>
+            <li><i class="fa fa-phone"></i> (555) 555-5555</li>
+            <li><i class="fa fa-envelope"></i> test@gmail.com</li>
+          </ul>
+        </div>
+        
+
+        <!-- CONTACT FORM -->
+        <div class="contact">
+          <h3 id="emailus">E-mail Us</h3>
+
+          <form id="contact-form">
+            <p>
+              <label>Name</label>
+              <input type="text" name="name" id="name" required />
+            </p>
+
+            <p>
+              <label>Last Name</label>
+              <input type="text" name="lname" id="lname" />
+            </p>
+
+            <p>
+              <label>E-mail Address</label>
+              <input type="email" name="email" id="email" required />
+            </p>
+
+            <p>
+              <label>Phone Number</label>
+              <input type="text" name="phone" id="phone" />
+            </p>
+
+            <p class="full">
+              <label>Message</label>
+              <textarea name="message" rows="6" id="message"></textarea>
+            </p>
+
+            <p class="full">
+              <button type="submit">Submit</button>
+            </p>
+          </form>
+          <!-- End #contact-form -->
+        </div>
+        <!-- End .contact -->
+      </div>
+      <!-- End .wrapper -->
+    </div>
+    <section class="sectionn2">
+      <div class="sec2title">
+        <h2 class="text-message">Location (Get directions)</h2>
+      </div>
+    </section>
+    <div class="map">
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2934.055664781214!2d21.15491881476714!3d42.660175779167844!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13549ee450c067bd%3A0x48d1a1d6f2a611ab!2sGaribaldi%2C%20Prishtin%C3%AB!5e0!3m2!1sen!2s!4v1609528684110!5m2!1sen!2s"
+        width="1142"
+        height="450"
+        frameborder="0"
+        style="border: 0"
+        allowfullscreen=""
+        aria-hidden="false"
+        tabindex="0"
+      ></iframe>
+    </div>
+    
+    <div class="feedback">
+      <h1 class="text-message">Give us your feedback</h1>
+      <div id="dragdrop1" class="drag-area" ondrop="drop(event)" ondragover="allowDrop(event)">
+        <img id="happy" draggable="true" ondragstart="drag(event)" src="./Images/contact/feedback/smiling.png">
+        <img id="normal" draggable="true" ondragstart="drag(event)" src="./Images/contact/feedback/confused.png">
+        <img id="sad" draggable="true" ondragstart="drag(event)" src="./Images/contact/feedback/sad.png">
+      </div>
+      <div id="dragdrop2" class="drop-area" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+      <div class="drag-info">
+        <p>Drag & Drop</p>
+        <img src="./Images/contact/feedback/dragdrop.gif" alt="">
+      </div>
+      <div class="text-area">
+        <div class="text-field">
+          <label for="feedback-text">Feedback:</label>
+          <textarea name="feedback-text" id="feedback-text" cols="30" rows="8"></textarea>
+        </div>
+        <div class="submit-field">
+          <label for="the-name">Name: </label>
+          <input type="text" name="the-name" id="the-name">
+          <button id="submit-feedback">Submit</button>
+        </div>
+
+      </div>
+    </div>
+    <br>
+    <div class="section3">
+      <h2 class="text-message">You can follow us here: </h2>
+    <div class="our-socials">
+        <div class="social"><a href="https://www.facebook.com" target="_blank"><img src="Images/contact/icons/facebook.png" ></a></div>
+        <div class="social"><a href="https://www.youtube.com" target="_blank"><img src="Images/contact/icons/youtube.png"></a></div>
+        <div class="social"><a href="https://www.twitter.com" target="_blank"><img src="Images/contact/icons/twitter.png"></a></div>
+        <div class="social"><a href="https://www.pinterest.com" target="_blank"><img src="Images/contact/icons/pinterest.png"></a></div> 
+    </div>    
+  </div>
+  
+    <!-- End .container -->
+  </main>
+  <br>
+ <!-- Change Color Based on Time --> 
+ <script>
+  function updateBackground() {
+  var 
+  hr = (new Date()).getHours(),
+  body = document.body,
+  bstyle = body.style,
+  headertopstyle = document.querySelector('.social-top'),
+      formstyle = document.querySelector('.feedback text-message'),
+  headertitle = document.querySelector('.homepage-description h1'),
+  headerdescription = document.querySelector('.homepage-description p'),
+  footer = document.querySelector('.main-footer');
+  
+  if (hr < 8) {
+    bstyle.backgroundColor = '#FFFFFF';
+    headertopstyle.style.backgroundColor = '#FFFFFF';
+    headertitle.style.color = '#3d3d3d';
+    headerdescription.style.color = '#3d3d3d';
+    // footer.style.backgroundColor = '#FFFFFF';
+    bstyle.color = '#3d3d3d';
+    document.querySelectorAll('.nav-item a').forEach(function(element) { element.style.color = '#3d3d3d'; });
+    document.querySelector('.mb-4').style.color = '#3d3d3d';
+          document.querySelector('.mb-5').style.color = '#3d3d3d';
+  } else if (hr > 8 && hr < 19) {
+    bstyle.backgroundColor = '#FFFFFF';
+    headertopstyle.style.backgroundColor = '#FFFFFF';
+    headertitle.style.color = '#3d3d3d';
+    headerdescription.style.color = '#3d3d3d';
+    // footer.style.backgroundColor = '#FFFFFF';
+    bstyle.color = '#3d3d3d';
+    document.querySelectorAll('.nav-item a').forEach(function(element) { element.style.color = '#3d3d3d'; });
+    document.querySelector('.mb-4').style.color = '#3d3d3d';
+          document.querySelector('.mb-5').style.color = '#3d3d3d';
+  } else {
+    bstyle.backgroundColor = '#3d3d3d';
+    headertopstyle.style.backgroundColor = '#3d3d3d';
+    headertitle.style.color = '#FFFFFF';
+    headerdescription.style.color = '#FFFFFF';
+    // footer.style.backgroundColor = '#000000';
+    bstyle.color = '#000';
+    document.querySelectorAll('.nav-item a').forEach(function(element) { element.style.color = '#FFFFFF' });
+    document.querySelector('.mb-4').style.color = '#FFFFFF';
+          document.querySelector('.mb-5').style.color = '#FFFFFF';
+    document.querySelectorAll('.dropdown-content').forEach(function(element) { element.style.backgroundColor = '#CCC'});
+  } 
+}
+
+  setInterval(updateBackground, 1000 * 60);
+  updateBackground();
+</script>
+
+
+
+
+  <footer>
+  <div class="main-footer">
+    <div id="contact-details" class="sector">
+      <h1>Contact Details </h1>
+      <pre>
+Freshness
+Dardania
+Prishtine
+10000
+      </pre>
+      <p>Tel: +383 45 123 456</p>
+      <p>Fax: 1234 56789</p>
+      <p>Email: <a href="mailto::username@domain.com">username@domain.com</a></p>
+    </div>
+    <div id="quick-links" class="sector">
+      <h1>Quick Links</h1>
+      <a href="#">Our specialities</a>
+      <hr class="sep">
+      <a href="#">Our wonderful staff</a>
+      <hr class="sep">
+      <a href="#">Make a reservation</a>
+      <hr class="sep">
+      <a href="#">Order now</a>
+      <hr class="sep">
+    </div>
+    <div id="from-the-blog" class="sector">
+      <h1>From The Blog</h1>
+      <pre>
+<strong>Preparing Tuna Fish</strong>
+<small>Posted by </small>Admin on 00.00.0000
+This is usually a coast side 
+dish with the calories needed
+to last throughout the day
+      </pre>
+      <a href="#" style="float: right;">See More &#187;</a>
+    </div>
+    <div id="newsletter" class="sector">
+      <h1>Newsletter</h1>
+      <input type="text" placeholder="Name">
+      <input type="text" placeholder="Email">
+      <input type="submit" style="font-weight: bold;">
+
+    </div>
+  </div>
+  <div class="end-footer">
+    <p>Copyright &copy; <sub>2013</sub> Domain Name - All Rights Reserved</p>
+    <p>Made By Group 10</p>
+  </div>
+</footer>
+</body>
+
+</html>
