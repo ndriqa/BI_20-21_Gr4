045f3abe58a882c1f3245448a95cbc6bbf6c72bb
Urata Sadiku <sadikuurata@gmail.com>
2021-05-17 18:19:24 +0200

    Use the str_split function

--- files/openFile.php
+++ files/openFile.php
@@ -3,10 +3,14 @@ $select = "Freshness Restaurant";
 if (empty($select))
     echo "Nothing to write";
     else{
-$file = fopen("/xampp/htdocs/BI_20-21_Gr4/files/openFile.txt", "a");
+//$file = fopen("/xampp/htdocs/BI_20-21_Gr4/files/openFile.txt", "a");
 //BY 'w' we specify that we will be writing to the file. If the file doesn’t exist, it’ll be created right away.
-//$file = fopen('/xampp/htdocs/BI_20-21_Gr4/files/openFile.txt', 'w'); 
-fwrite($file, "\n");
-fwrite($file  , $select);
+$file = fopen('/xampp/htdocs/BI_20-21_Gr4/files/openFile.txt', 'w'); 
+$pieces = str_split($select, 1024 * 4);
+foreach($pieces as $piece){
+    fwrite($file,$piece,strlen($piece));
+}
+//fwrite($file, "\n");
+//fwrite($file  , $select);
 echo "data successfully entered";
 fclose($file );}
