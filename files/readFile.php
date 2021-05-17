bf84a258a9b3007c95b07e38bdbdff66a134edec
Urata Sadiku <sadikuurata@gmail.com>
2021-05-17 15:33:15 +0200

    Read from a file in php

---
+++ files/readFile.php
@@ -0,0 +1,7 @@
+<?php
+$file = fopen('/xampp/htdocs/BI_20-21_Gr4/files/openFile.txt', 'r');
+$contents = fread($file, filesize('/xampp/htdocs/BI_20-21_Gr4/files/openFile.txt'));
+fclose($file);
+//$contents = file_get_contents('/xampp/htdocs/BI_20-21_Gr4/files/openFile.txt');
+
+?>
