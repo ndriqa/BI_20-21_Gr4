c942ad7af97611ba06a7c2f38a499b1f76f2c390
Urata Sadiku <sadikuurata@gmail.com>
2021-05-17 02:18:16 +0200

    Update mail form

--- mail.php
+++ mail.php
@@ -9,10 +9,10 @@ if(isset($_POST['name']) && isset($_POST['lname']) && isset($_POST['email']) && 
 	$message=$_POST['message'];
 	
 	//$mysql="insert into contact_us(name,lname,email,phone,message) values('$name','$lname','$email','$phone','$message')";
-	//$msg="Thanks message";
+	$msg="Thanks message";
   
 	
-	$html="<table><tr><td>Name</td><td>$name</td></tr><tr><td>Email</td><td>$lname</td></tr><tr><td>Email</td><td>$email</td></tr><tr><td>Mobile</td><td>$mobile</td></tr><tr><td>Comment</td><td>$message</td></tr></table>";
+	$html="<table><tr><td>Name</td><td>$name</td></tr><tr><td>Last Name</td><td>$lname</td></tr><tr><td>E-mail Address</td><td>$email</td></tr><tr><td>Phone Number</td><td>$mobile</td></tr><tr><td>Message</td><td>$message</td></tr></table>";
 	
 	include('PHPMailer/PHPMailerAutoload.php');
 	$mail=new PHPMailer(true);
